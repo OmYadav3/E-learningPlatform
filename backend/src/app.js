28447c33e59dc8aa1fa153efd74b63ae87ba8845
OmYadav3 <omydv3@gmail.com>
2025-08-26 20:59:16 +0530

    upgrate backend basic setup

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+
+/* ======MIDDLEWARE ======== */
+app.use(cors(
+    {
+        origin:process.env.CORS_ORGIN,
+        credentials:true,
+    }
+));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static('public'));
+app.use(cookieParser());
+
+
+
+export { app };
