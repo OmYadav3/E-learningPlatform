c898ccf87fe3d3ddd305a2a47d2550d2c19cc034
OmYadav3 <omydv3@gmail.com>
2025-08-27 22:26:14 +0530

    update index.js file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,15 @@
-import dotenv from 'dotenv'
-import app from './app.js'
+import dotenv from "dotenv"
+import {app} from "./app.js"
+import connectionDb from "./db/db.js"
 
 dotenv.config({
    path:'./.env'
 })
 
-app.listen(process.env.PORT || 3000, () => {
+
+connectionDb();
+
+app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running at port ${process.env.PORT}`);
       
    } )
