e5a019fb698e5df180ebfd0b0eff66e949c52c0a
OmYadav3 <omydv3@gmail.com>
2025-09-24 21:10:57 +0530

    add the couse model

---
+++ backend/src/models/course.model.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema(
+  {
+    
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Course = mongoose.model("courses", courseSchema);
